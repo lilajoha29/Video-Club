6e9730007ee7281d13c66b9c77bc87cf2e14d2cf
María F. Herrera G <mariaf.herrera@outlook.com>
2023-02-08 12:41:48 -0500

    MovieList comp. development with responsive

--- public/src/App.jsx
+++ public/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import MovieList from './components/MovieList'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App h-screen bg-cover bg-default">
-
+    <div className="App h-full bg-cover bg-default">
+      <MovieList/>
 
     </div>
   )
