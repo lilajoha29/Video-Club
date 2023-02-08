55383efa57506b1aaf9933642ee730262290c0a2
Jajaharold <haroldurrea@gmail.com>
2023-02-08 15:17:59 -0500

    Add movie section & component routing

--- public/src/components/AddMovie.jsx
+++ public/src/components/AddMovie.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const addMovie = () => {
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [phone, setPhone] = useState('')
+
   return (
     <div>
+      <h1 className='font-Quicksand font-bold text-xl md:text-3xl text-lightYellow text-center py-5 md:p-10'>Agregar nueva película</h1>
 
+      <section>
+        <div className= 'grid gap-2 bg-darkRed/60 w-10/12 md:w-4/12 m-auto py-2.5 md:py-5 rounded-md grid-cols-1 '>
+          <input className='text-center py-1 mx-2.5 md:mx-5 rounded-md  font-Quicksand text-lightBlue font-bold border-2 border-darkBlue' type="text" placeholder='Escriba el nombre de la película' />
+          <input className='text-center py-1 mx-2.5 md:mx-5 rounded-md  font-Quicksand text-lightBlue font-bold border-2 border-darkBlue' type="text" placeholder='Escriba el año de lanzamiento de la película' />
+          <input className='text-center py-1 mx-2.5 md:mx-5 rounded-md  font-Quicksand text-lightBlue font-bold border-2 border-darkBlue' type="text" placeholder='Escriba la duración de la película' />
+          <input className='text-center py-1 mx-2.5 md:mx-5 rounded-md  font-Quicksand text-lightBlue font-bold border-2 border-darkBlue' type="text" placeholder='Escriba el idioma original de la película' />
+          <input className='text-center py-1 mx-2.5 md:mx-5 rounded-md  font-Quicksand text-lightBlue font-bold border-2 border-darkBlue' type="text" placeholder='Escriba la fecha de lanzamiento (dd/mm/aaaa)' />
+          <input className='text-center py-1 mx-2.5 md:mx-5 rounded-md  font-Quicksand text-lightBlue font-bold border-2 border-darkBlue' type="text" placeholder='Escriba el país de origen de la película' />
+
+        </div>
+        <div className='w-9/12 m-auto py-5 flex justify-around'>
+          <button className='font-Quicksand font-semibold md:text-xl px-4 md:px-8 py-1 text-center text-white hover:text-darkRed bg-lightBlue hover:bg-lightYellow rounded-md border-2 border-darkBlue hover:border-darkYellow'>AGREGAR</button>
+        </div>
+      </section>
     </div>
   )
 }
--- public/src/components/MovieList.jsx
+++ public/src/components/MovieList.jsx
@@ -8,7 +8,7 @@ const movieList = () => {
       <h1 className='font-Quicksand font-bold text-xl md:text-3xl text-lightYellow text-center py-5 md:p-10'>Lista de películas</h1>
       <div className='grid grid-cols-1 lg:grid-cols-2'>
       <section>
-        <box className= 'grid gap-2 bg-darkRed/60 w-10/12 m-auto py-2.5 md:py-5 rounded-md'>
+        <div className= 'grid gap-2 bg-darkRed/60 w-10/12 m-auto py-2.5 md:py-5 rounded-md'>
           <div className='grid grid-cols-4 bg-white mx-2.5 md:mx-5 rounded-md border-2 border-lightYellow text-lightBlue'>
             <h1 className='col-start-1 col-end-2 font-Quicksand font-semibold px-3 text-center md:text-xl md:py-0.5'>ID</h1>
             <h2 className='col-start-2 col-end-5 font-Quicksand font-normal px-3 text-sm md:text-xl my-auto'>123456789</h2>
@@ -61,7 +61,7 @@ const movieList = () => {
               <h3 className='font-Quicksand font-normal px-1.5 md:px-3 text-sm md:text-xl my-0.5 md:my-1 break-all'>Estados Unidos</h3>
             </div>
           </div>
-        </box>
+        </div>
         <div className='w-9/12 m-auto py-5 flex justify-around'>
           <button className='font-Quicksand font-semibold md:text-xl px-4 md:px-8 py-0.5 md:py-1 text-center text-white hover:text-darkRed bg-lightBlue hover:bg-lightYellow rounded-md border-2 border-darkBlue hover:border-darkYellow'>EDITAR</button>
           <button className='font-Quicksand font-semibold md:text-xl px-4 md:px-8 py-0.5 text-center text-white hover:text-darkRed bg-lightBlue hover:bg-lightYellow rounded-md border-2 border-darkBlue hover:border-darkYellow'>ELIMINAR</button>
