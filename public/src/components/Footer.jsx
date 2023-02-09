3112198111815c7932b4b257fec82994aaa53255
danilodiaz <diazdanilo340@gmail.com>
2023-02-08 21:57:20 -0500

    commit

--- public/src/components/Footer.jsx
+++ public/src/components/Footer.jsx
@@ -3,7 +3,16 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-
+      <div>
+        <div className=''>
+          <h3>Desarrollado por:</h3>
+          <a href="https://github.com/lilajoha29">lilajoha29</a>
+          <a href="https://github.com/MariaHerrera03">MariaHerrera03</a>
+          <a href="https://github.com/Alexis1326">Alexis1326</a>
+          <a href="https://github.com/H4r0l">H4r0l</a>
+          <a href="https://github.com/DaniloDiaz08">DaniloDiaz08</a>
+        </div>
+      </div>
     </div>
   )
 }
